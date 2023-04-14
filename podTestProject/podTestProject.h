b527e38424fece631ebc20625832a34a342ec6fb
sjpark <sjpark@avatye.com>
2023-04-14 18:37:54 +0900

    init

---
+++ podTestProject/podTestProject.h
@@ -0,0 +1,18 @@
+//
+//  podTestProject.h
+//  podTestProject
+//
+//  Created by 박상준 on 2023/04/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podTestProject.
+FOUNDATION_EXPORT double podTestProjectVersionNumber;
+
+//! Project version string for podTestProject.
+FOUNDATION_EXPORT const unsigned char podTestProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podTestProject/PublicHeader.h>
+
+
